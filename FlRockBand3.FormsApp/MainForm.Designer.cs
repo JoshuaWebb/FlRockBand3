bad29d1742fdb95a92e663c0926f289fdab3fdd2
Joshua Webb <bbew.auhsoj+git@gmail.com>
2017-06-05 00:09:07 +1000

    Incredibly primitive GUI

---
+++ FlRockBand3.FormsApp/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FlRockBand3.FormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.filePathBox = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(13, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 207);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ConvertButtonClick);
+            // 
+            // filePathBox
+            // 
+            this.filePathBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filePathBox.Location = new System.Drawing.Point(94, 15);
+            this.filePathBox.Name = "filePathBox";
+            this.filePathBox.Size = new System.Drawing.Size(178, 20);
+            this.filePathBox.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "MIDI Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SelectPathButtonClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.filePathBox);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox filePathBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FlRockBand3.FormsApp/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlRockBand3.FormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Load += Form_OnLoad;
+        }
+
+        private void Form_OnLoad(object sender, EventArgs e)
+        {
+            AllowDrop = true;
+            DragEnter += Form_DragEnter;
+            DragDrop += Form_DragDrop;
+        }
+
+        void Form_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop))
+                return;
+
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length == 1)
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            filePathBox.Text = files[0];
+        }
+
+        private async void ConvertButtonClick(object sender, EventArgs e)
+        {
+            var midiPath = new FileInfo(filePathBox.Text);
+
+            var convert = Task.Run(() =>
+            {
+                var fileDir = midiPath.Directory.FullName;
+                var inFileName = Path.GetFileNameWithoutExtension(midiPath.Name);
+                var outFilePath = Path.Combine(fileDir, Path.ChangeExtension(inFileName, "txt"));
+                var fixedOutFilePath = Path.Combine(fileDir, Path.ChangeExtension(inFileName + "_clean", "mid"));
+                var fixedOutFilePathTxt = Path.ChangeExtension(fixedOutFilePath, "txt");
+
+                var dumper = new Dumper();
+                using (var fs = new FileStream(outFilePath, FileMode.Create))
+                    dumper.Dump(midiPath.FullName, fs);
+
+                var fixer = new MidiFixer();
+                fixer.Fix(midiPath.FullName, fixedOutFilePath);
+
+                using (var fs = new FileStream(fixedOutFilePathTxt, FileMode.Create))
+                    dumper.Dump(fixedOutFilePath, fs);
+            });
+
+            try
+            {
+                await convert;
+                MessageBox.Show("Done", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show(ioe.Message, "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show(fe.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SelectPathButtonClick(object sender, EventArgs e)
+        {
+            var initialDir = @"C:\";
+            if (!string.IsNullOrEmpty(filePathBox.Text))
+                initialDir = filePathBox.Text;
+
+            var openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = initialDir,
+                Filter = @"MIDI files (*.mid)|*.mid|All files (*.*)|*.*",
+                FilterIndex = 0,
+                RestoreDirectory = false,
+                Multiselect = false
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePathBox.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
