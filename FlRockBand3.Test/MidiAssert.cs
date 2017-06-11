8b4e18d0431970037b27a66026777362f2e68c32
Joshua Webb <bbew.auhsoj+git@gmail.com>
2017-06-11 18:02:27 +1000

    Add more information to failed MidiAssertions

--- FlRockBand3.Test/MidiAssert.cs
+++ FlRockBand3.Test/MidiAssert.cs
@@ -6,24 +6,27 @@ namespace FlRockBand3.Test
 {
     public static class MidiAssert
     {
+        private static void BaseEqual(MidiEventCollection expected, MidiEventCollection actual)
+        {
+            Assert.That(actual.Tracks, Is.EqualTo(expected.Tracks), "Number of tracks not equal.");
+            Assert.That(actual.DeltaTicksPerQuarterNote, Is.EqualTo(expected.DeltaTicksPerQuarterNote), "PPQ not equal.");
+            Assert.That(actual.MidiFileType, Is.EqualTo(expected.MidiFileType), $"{nameof(actual.MidiFileType)} not equal.");
+        }
+
         public static void Equal(MidiEventCollection expected, MidiEventCollection actual)
         {
-            var comparer = new MidiEventEqualityComparer();
+            BaseEqual(expected, actual);
 
-            Assert.That(actual.Tracks, Is.EqualTo(expected.Tracks));
-            Assert.That(actual.DeltaTicksPerQuarterNote, Is.EqualTo(expected.DeltaTicksPerQuarterNote));
-            Assert.That(actual.MidiFileType, Is.EqualTo(expected.MidiFileType));
+            var comparer = new MidiEventEqualityComparer();
             for (var i = 0; i < expected.Tracks; i++)
-                Assert.That(actual[i], Is.EqualTo(expected[i]).Using(comparer));
+                Assert.That(actual[i], Is.EqualTo(expected[i]).Using(comparer), $"Track #{i} is different.");
         }
 
         public static void Equivalent(MidiEventCollection expected, MidiEventCollection actual)
         {
-            var comparer = new MidiEventEqualityComparer();
+            BaseEqual(expected, actual);
 
-            Assert.That(actual.Tracks, Is.EqualTo(expected.Tracks));
-            Assert.That(actual.DeltaTicksPerQuarterNote, Is.EqualTo(expected.DeltaTicksPerQuarterNote));
-            Assert.That(actual.MidiFileType, Is.EqualTo(expected.MidiFileType));
+            var comparer = new MidiEventEqualityComparer();
             Assert.That(actual.Select(t => t), Is.EquivalentTo(expected.Select(t => t)).Using(comparer));
         }
     }
